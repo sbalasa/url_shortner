61ecb9c2728b308ded0eb69579dce6fb29a1c877
Santhosh Balasa <santhosh.kbr@gmail.com>
2021-05-29 13:41:27 +0000

    Adding pytest fixture to pass the long url

--- test_shorten_url.py
+++ test_shorten_url.py
@@ -6,11 +6,16 @@ from shorten_url import get_cached_urls, encode
 ecoded_url = ""
 
 
-def test_encode():
+@pytest.fixture
+def long_url():
+    return "http://testing.com/url_shortening_test"
+
+
+def test_encode(long_url):
     global encoded_url
-    encoded_url = encode("http://testing.com/url_shortening_test")
+    encoded_url = encode(long_url)
     assert 8 == len(encoded_url)
 
 
-def test_cached_urls():
-    assert {f"{encoded_url}": "http://testing.com/url_shortening_test"} == get_cached_urls()
+def test_cached_urls(long_url):
+    assert {f"{encoded_url}": long_url} == get_cached_urls()
