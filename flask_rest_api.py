1e5db075c62ff95c77e1463e4d7b73314938daef
Santhosh Balasa <santhosh.kbr@gmail.com>
2021-06-02 06:11:19 +0000

    Adding REST API service support for json output

---
+++ flask_rest_api.py
@@ -0,0 +1,20 @@
+from shorten_url import encode
+from flask_restful import Api, Resource
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@api.resource("/")
+class ShortenURL(Resource):
+    def post(self):
+        long_url = request.get_json(force=True)
+        return jsonify(
+            {"long_url": long_url["long_url"], "shorten_url": encode(long_url)}
+        )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
